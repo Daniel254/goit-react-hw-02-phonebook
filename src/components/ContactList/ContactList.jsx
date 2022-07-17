818ab9365e163402f7601f41e69bdb04f3be90a4
Daniel254 <Daniel.laskin@gmail.com>
2022-07-17 10:26:23 +0300

    Step 3 done

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -3,6 +3,27 @@ import React, { Component } from 'react';
 import Notification from 'components/Notification';
 import Section from 'components/Section';
 import { List } from './ContactList.styled';
+import SearchContact from 'components/SearchContact';
+import sanitizeString from 'utils/sanitizeString';
+
+function FilteredList({ filter, listItems, message, filterHandler }) {
+  return (
+    <>
+      <SearchContact filterHandler={filterHandler} filterString={filter} />
+      {listItems.length > 0 ? (
+        <List>
+          {listItems.map(({ id, name, number }) => (
+            <li key={id}>
+              {name}: {number}
+            </li>
+          ))}
+        </List>
+      ) : (
+        <Notification message={message} />
+      )}
+    </>
+  );
+}
 
 export default class ContactList extends Component {
   static propTypes = {
@@ -14,19 +35,32 @@ export default class ContactList extends Component {
       })
     ),
   };
+  state = {
+    filter: '',
+  };
+
+  filterHandler = ({ currentTarget: { value } }) => {
+    this.setState({
+      filter: sanitizeString(value),
+    });
+  };
 
   render() {
     const { contactList } = this.props;
+    const { filter } = this.state;
+    const filteredContactList = contactList.filter(item =>
+      sanitizeString(item.name).includes(filter)
+    );
+
     return (
       <Section title="Contacts" mt="10px">
         {contactList.length > 0 ? (
-          <List>
-            {contactList.map(({ id, name, number }) => (
-              <li key={id}>
-                {name}: {number}
-              </li>
-            ))}
-          </List>
+          <FilteredList
+            filter={filter}
+            filterHandler={this.filterHandler}
+            listItems={filteredContactList}
+            message="No contacts found"
+          />
         ) : (
           <Notification message="No contacts" />
         )}
